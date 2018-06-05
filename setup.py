e9e6185b8c54b82f8b567d8baf9f9319a6a3ce04
Jens Vagelpohl <jens@netz.ooo>
2018-06-05 14:04:32 +0200

    - update trove classifiers

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
+##############################################################################
+#
+# Copyright (c) 2001 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 from setuptools import setup, find_packages
 
 setup(
@@ -9,9 +22,7 @@ setup(
     author_email="zms@sntl-publishing.com",
     url="https://github.com/zms-publishing/Products.ZMySQLDA",
     description="MySQL Zope adapter.",
-    long_description=(
-        "MySQL Database Adapter for Zope."
-    ),
+    long_description="MySQL Database Adapter for Zope.",
     classifiers=[
         "Development Status :: 6 - Mature",
         "Environment :: Web Environment",
@@ -22,7 +33,11 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Database",
+        "Topic :: Database :: Front-Ends",
     ],
     packages=find_packages(),
     include_package_data=True,
