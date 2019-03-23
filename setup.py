32a1aa75e1c834bbf71651af23824ac26f305d45
Jens Vagelpohl <jens@netz.ooo>
2019-03-23 11:16:11 -0500

    remove explicit ``setuptools`` version pin in ``setup.py`` (fixes #11)

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     zip_safe=False,
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     install_requires=[
-        'setuptools >= 36.2',
+        'setuptools',
         'six',
         'mysqlclient',
         'Products.ZSQLMethods',
