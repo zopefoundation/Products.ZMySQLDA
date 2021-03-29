aa21ba54735d5a67f963628ab9fb12e7f36284a5
Jens Vagelpohl <jens@netz.ooo>
2021-03-29 14:21:56 +0200

    - brute-forcing install_requires [ci skip]

--- setup.py
+++ setup.py
@@ -17,6 +17,18 @@ from setuptools import find_packages
 from setuptools import setup
 
 
+install_requires=[
+    'setuptools',
+    'six',
+    'Products.ZSQLMethods',
+]
+if not os.environ.get('READTHEDOCS') == 'True':
+    install_requires += [
+    'mysqlclient < 2;python_version < "3"',
+    'mysqlclient;python_version >= "3"',
+],
+
+
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
@@ -65,13 +77,7 @@ setup(
     namespace_packages=['Products'],
     zip_safe=False,
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
-    install_requires=[
-        'setuptools',
-        'six',
-        'Products.ZSQLMethods',
-        'mysqlclient < 2;python_version < "3"',
-        'mysqlclient;python_version >= "3"',
-    ],
+    install_requires=install_requires,
     extras_require={
         'docs': ['Sphinx', 'repoze.sphinx.autointerface'],
     },
