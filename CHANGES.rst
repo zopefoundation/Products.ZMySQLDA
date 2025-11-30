810d5c0e6127a9ee4ac47bc174042fcf0623ee4d
Jens Vagelpohl <jens@plyp.com>
2025-11-30 17:43:13 +0100

    - Move package metadata from setup.py to pyproject.toml

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,6 +4,8 @@ Products.ZMySQLDA change log
 6.3 (unreleased)
 ----------------
 
+- Move package metadata from setup.py to pyproject.toml.
+
 
 6.2 (2025-11-20)
 ----------------
--- setup.py
+++ setup.py
@@ -11,63 +11,9 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-import os
 
 from setuptools import setup
 
 
-install_requires = [
-    'Products.ZSQLMethods',
-    'Zope',
-]
-if not os.environ.get('READTHEDOCS') == 'True':
-    install_requires += [
-        'mysqlclient',
-    ]
-
-
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
-
-setup(
-    name='Products.ZMySQLDA',
-    version='6.3.dev0',
-    license='ZPL-2.1',
-    author='Zope Foundation and Contributors',
-    author_email='zope-dev@zope.dev',
-    url='https://zmysqlda.readthedocs.io',
-    project_urls={
-        'Documentation': 'https://zmysqlda.readthedocs.io',
-        'Issue Tracker': ('https://github.com/zopefoundation'
-                          '/Products.ZMySQLDA/issues'),
-        'Sources': 'https://github.com/zopefoundation/Products.ZMySQLDA',
-    },
-    description='MySQL Zope adapter.',
-    long_description=read('README.rst') + '\n' + read('CHANGES.rst'),
-    long_description_content_type='text/x-rst',
-    classifiers=[
-        'Development Status :: 6 - Mature',
-        'Environment :: Web Environment',
-        'Framework :: Zope',
-        'Framework :: Zope :: 5',
-        'License :: OSI Approved :: Zope Public License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Database',
-        'Topic :: Database :: Front-Ends',
-    ],
-    python_requires='>=3.10',
-    install_requires=install_requires,
-    extras_require={
-        'docs': ['Sphinx', 'furo'],
-    },
-    include_package_data=True,
-)
+# See pyproject.toml for package metadata
+setup()
